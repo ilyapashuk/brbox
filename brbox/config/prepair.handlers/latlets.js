2a5816e6f4554e4c6b185f8deb45caa44da549ff
ilyapashuk <ilusha.paschuk@gmail.com>
2021-08-11 16:20:58 +0300

    latlets improved

--- brbox/config/prepair.handlers/latlets.js
+++ brbox/config/prepair.handlers/latlets.js
@@ -9,38 +9,56 @@ obj[chr] = true
 return obj
 }
 let latobj = buildchobj(latlets)
+let upperobj = {}
 Object.keys(latobj).forEach(function(key) {
-latobj[key.toUpperCase()] = true
+let uk = key.toUpperCase()
+latobj[uk] = true
+upperobj[uk] = true
 })
 function isLatlet(l) {
 return (l in latobj)
 }
 function isupper(l) {
-return l === l.toUpperCase()
+return (l in upperobj)
 }
 
+function getchardesc(chr) {
+if (isLatlet(chr)) {
+if (isupper(chr)) {
+return 2
+} else {
+return 1
+}
+} else {
+return 0
+}
+}
 
 let pm = charForDots("6")
 let pb = charForDots("46")
 
 function handle(line,opts) {
-let latm = false
+let cd = 0
 // строка разбивается на символы
 let chars = line.split("")
 return chars.map(function(char) {
 if (char === ' ') {
 return char
 }
-if (! isLatlet(char)) {
-latm = false
-return char
-} else {
-if (! latm) {
-latm = true
-return (isupper(char) ? pb : pm) + char
-} else {
-return char
+let cdd = getchardesc(char)
+if (cdd > cd) {
+cd = cdd
+switch (cdd) {
+case 2:
+return pb + char
+break
+case 1:
+return pm + char
+break
 }
+} else {
+cd = cdd
+return char
 }
 }).join("")
 }
